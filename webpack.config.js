5ece5499f46b6be207cf9bc0661a94a2152cfac5
jakejooyoung <jakejooyoung@gmail.com>
2017-03-20 16:58:37 -0700

    Add UglifyPlugin to webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,20 @@
 // In webpack.config.js
+
+var webpack = require('webpack')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
+var UglifyJsPlugin = require('uglify-js-plugin')
 var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
   template: __dirname + '/client/index.html',
   filename: 'index.html',
-  inject: 'body'
+  inject: 'body',
+  links: [
+    'https://fonts.googleapis.com/css?family=Roboto',
+  ]
+});
+var UglifyJsPluginConfig = new UglifyJsPlugin({
+  compress: {
+      warnings: false
+  }
 });
 module.exports = {
   entry: [
@@ -19,6 +30,18 @@ module.exports = {
       { test: /\.scss$/, loaders: ['style-loader', 'css-loader','sass-loader' ], exclude: /node_modules/  }
     ]
   },
-  plugins: [HTMLWebpackPluginConfig]
+  performance: {
+    hints: "warning", // enum
+    maxAssetSize: 200000, // int (in bytes),
+    maxEntrypointSize: 400000, // int (in bytes)
+    assetFilter: function(assetFilename) { 
+      // Function predicate that provides asset filenames
+      return assetFilename.endsWith('.css') || assetFilename.endsWith('.js');
+    }
+  },
+  devtool: "source-map", // enum
+  // enhance debugging by adding meta info for the browser devtools
+  // source-map most detailed at the expense of build speed.
+  plugins: [HTMLWebpackPluginConfig,UglifyJsPluginConfig]
 };
 
