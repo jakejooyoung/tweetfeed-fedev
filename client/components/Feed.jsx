1ad0384f9b0ea652e8627ed526ef414b446e4fdd
jakejooyoung <jakejooyoung@gmail.com>
2017-03-24 05:33:54 -0700

    Implement recursively repeating component. Categorized Boards for domain listings.

--- client/components/Feed.jsx
+++ client/components/Feed.jsx
@@ -40,71 +40,69 @@ var categories= [
 ];
 const domains=Domains.DomainList.Domain;
 const domainsByCategory=[];
-categories.forEach(function(category){
-	domainsByCategory[category]=[];
-});
-
+function parseDomainArr(){
+	categories.forEach(function(category){
+		domainsByCategory[category]=[];
+	});
+	domains.map(function(obj){
+		var dn={
+			Name:obj._Name,
+			Category:''
+		};
+		categories.forEach(function(ctg){
+			// Domain has matching category 
+			if (dn.Name.includes(ctg)){
+				// Domain was not inserted into matching category array
+				// or category assigned was less specific than new category	
+				// TO-DO: rearrange categories by length.
+				if (!dn.Category||dn.Category.length<ctg.length){
+					// Previously inserted to byCategory array
+					if (dn.Category){
+						// Delete from old category array
+						var index=domainsByCategory[dn.Category].indexOf(dn);
+						domainsByCategory[dn.Category].splice(index,1);
+					}
+					// In all other cases assign dn.Category and insert to byCategory
+					dn.Category=ctg;
+					domainsByCategory[ctg].push(dn);
+				}
+			}
+		});
+		return dn;
+	});
+}
+// domain names saved as objects in an array
+// [[k,{Name:x,Category:y}],[k,{Name:x,Category:y}]]
+parseDomainArr();
 
 export class Feed extends React.Component {
 	renderTitle(){ 
 		return <NpUI.Title user='Jake'/>
 	}
-	parseDomainArr(){
-		var arr= domains.map(function(obj){
-			var dn={
-				Name:obj._Name,
-				Category:''
-			};
-			categories.forEach(function(ctg){
-				// Domain has matching category 
-				if (dn.Name.includes(ctg)){
-					// Domain was not inserted into matching category array
-					// or category assigned was less specific than new category	
-					// TO-DO: rearrange categories by length.
-					if (!dn.Category||dn.Category.length<ctg.length){
-						// Previously inserted to byCategory array
-						if (dn.Category){
-							// Delete from old category array
-							var index=domainsByCategory[dn.Category].indexOf(dn);
-							domainsByCategory[dn.Category].splice(index,1);
-						}
-						// In all other cases assign dn.Category and insert to byCategory
-						dn.Category=ctg;
-						domainsByCategory[ctg].push(dn);
-					}
-				}
-			});
-			return dn;
+
+	renderBoard(){
+		let items=[];
+
+		categories.forEach(function(category){
+			let parr=domainsByCategory[category];
+			items.push(
+				<div key={category} className='post'>
+					{category}
+					<Repeat className='feed' numTimes={parr.length} >
+		   		   		{(index) => <Blip key={index} content={parr[index].Name} />}
+		   			</Repeat>
+				    <NpUI.Button action='yes'/>
+				</div>
+			);
 		});
-		// return arr;
-		domainsByCategory['bitcoin'].forEach(function (c){ console.log(c)});	
-		domainsByCategory['mbtc'].forEach(function (c){ console.log(c)});	
-		domainsByCategory['btc'].forEach(function (c){ console.log(c)});
-		console.log(domainsByCategory['bitcoin']);
-		return arr;
-	}
-	// dynamicSort(category) {
-	//     var arr=[];
-	//     for (i=0;i<categories.length;i++){
-	//     	var catArr=[];
-	//     	feed.map
-	//     }
-	// }
-	renderPosts(){
-		// var feed=[];
-		// feed=Domains.DomainList.Domain;
-		var feed=(this.props.type=='domains') ? this.parseDomainArr():['fdf','fdfd','shihi'];
-		return (
-			<Repeat className='feed' numTimes={feed.length} >
-	   		   	{(index) => <Post key={index} content={feed[index].Name} />}
-		    </Repeat>
-   		)
+
+		return items;
 	}
 	render() {
     	return (
     		<div id='feed-container'>	
 				{this.renderTitle()}
-				{this.renderPosts()}
+				{this.renderBoard()}
 			</div>
 		);
  	}
@@ -125,9 +123,18 @@ export class Post extends React.Component {
 	render(){
 		return (
 			<div className='post'>
-				<h4>{this.props.reactKey} </h4>
+				<h4>{this.key} </h4>
 				<p> {this.props.content} </p>
 			    {this.renderButton()}
+			</div>
+		);
+	}
+}
+export class Blip extends React.Component {
+	render(){
+		return (
+			<div className='bilp'>
+				<p> {this.props.content} </p>
 			</div>
 		);
 	}
