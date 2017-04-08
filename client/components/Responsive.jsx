f7bd85daeb13bc7123d130cd073326a2e27e52ae
Jake Kim <jake@npgains.com>
2017-04-07 17:04:10 -0700

    Update ui. Pinterest Layout should be centered within main div. Set margin to 0 auto, and width to 90%

--- client/components/Responsive.jsx
+++ client/components/Responsive.jsx
@@ -3,9 +3,13 @@ import StackGrid from "react-stack-grid";
 
 export default class Responsive extends React.Component {
   render() {
+    const center={
+      "width": "90%",
+      "margin": "0 auto"
+    }
     return (
       <StackGrid
-        columnWidth={150}
+        columnWidth={150} style={center}
       >
         <div className="card" key="key1">Item 1</div>
         <div className="card" key="key2">Item 2</div>
