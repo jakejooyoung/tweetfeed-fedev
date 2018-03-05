3611f77b730ecb0d25ae1cd742b6b8c067dc4383
jknpg <jake@npgains.com>
2018-03-05 12:56:39 -0800

    Add skeleton css and create separate folder for css files.

--- client/main.js
+++ client/main.js
@@ -4,8 +4,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
-import './components/app.scss';
 
+import './css/normalize.css';
+import './css/skeleton.css';
+import './css/app.scss';
 const app = <App />;
 
 ReactDOM.render(app, document.getElementById('app-container'));
