4571b0a88c13fe4dcc9a3857c0ee5558b0aba1fe
jakejooyoung <jakejooyoung@gmail.com>
2018-03-16 21:30:46 -0700

    Remove unnecessary components from App.jsx.

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,55 +3,23 @@
 */
 import React from "react";
 import ReactDOM from "react-dom";
-import Menu from "./Menu.jsx";
 import Responsive from "./Responsive.jsx";
-import CategorizedList from "./CategorizedList.jsx"
-import Article from "./Article.jsx"
 
 export default class App extends React.Component {
 	constructor(props) {
 		super(props);
 	}
 	render() {
+		const mainStyle = {"border":"1px solid grey"};
 		return (
 			<div className="app">
-				{this.renderMain()}
-				{this.renderSide()}
+				<div className="main" style={mainStyle}>
+					<h1> Barebone TweetFeed </h1>
+					<Responsive/>
+				</div>
 			</div>
 		);
 	}
-	renderMain(){
-		const mainStyle = {"order":(this.props.menuOnRight?0:1)};
-		return (
-			<div className="main" style={mainStyle}>
-				<Article/>
-				<Responsive/>
-			</div>
-		)
-	}
-	renderSide(){
-		const sideStyle = { 
-			"order":(this.props.menuOnRight?1:0), 
-			"minWidth":this.props.menuWidth 
-		};
-		return (
-			<div className="side" style={sideStyle}>
-				<Menu 
-					fixedTop={this.props.fixedTop}
-					fixedHeight={80}
-					width={sideStyle.minWidth} 
-					menuOnRight={this.props.menuOnRight} 
-					isCollapsed> 
-					<CategorizedList fixedTop={this.props.fixedTop} wrapperType="post"/>
-		  		</Menu>
-			</div>
-		)
-	}
-}
-App.defaultProps={
-	menuOnRight:false,
-	menuWidth:400,
-	fixedTop:true
 }
 
 
